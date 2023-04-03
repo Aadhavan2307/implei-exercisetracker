9556402b4da8e0e6252aee10c70b4a7376c570bc
Aadhavan2307 <aadhavan.shruthi@gmail.com>
2023-04-03 10:58:06 +0530

    project update 2

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ app.get('/', (req, res) => {
 });
 
 // User Database
-let users = [{Username:"Implei", Log:[{description:"hello",duration:15,date:"Fri Dec 25 2015"},{description:"bye",duration:15,date:"Fri Dec 25 2015"}]}];
+let users = [{username:"Implei", _id:"0", log:[{description:"hello",duration:15,date:"Fri Dec 25 2015"},{description:"bye",duration:15,date:"Fri Dec 25 2015"}]}];
 
 // Create New User Function
 app.post("/api/users", function(req, res) {
   let username = req.body.username;
-  if (users.find(user => user.Username == username)) {
+  if (users.find(user => user.username == username)) {
     // If User Already Exists
-    let index = users.findIndex(u => u.Username == username)
-    res.json({ "username": users[index].Username, "_id": index });
+    let index = users.findIndex(u => u.username == username)
+    res.json({ "username": users[index].username, "_id": index });
   } else {
     // If New User is to be Added
-    users.push({Username:username, Log:[]});
-    let index = users.findIndex(u => u.Username == username)
-    res.json({ "username": users[index].Username, "id": index });
+    users.push({username:username, _id:users.length.toString(), log:[]});
+    let index = users.findIndex(u => u.username == username)
+    res.json({ "username": users[index].username, "_id": index });
   }
 });
 
@@ -36,20 +36,32 @@ app.post("/api/users/:_id/exercises", function(req, res) {
   let id = req.params._id;
   let user = users[id];
   let description = req.body.description;
-  let duration = req.body.duration;
+  let duration = parseInt(req.body.duration);
   let date = new Date(req.body.date);
   date = date.toDateString();
-  user.Log.push({description:description,duration:duration,date:date});
-  res.json({"_id":id,"username":user.Username,"date":date,"duration":duration,"description":description});
+  if (date == "Invalid Date"){
+    date = new Date().toDateString();
+  }
+  user.log.push({description:description,duration:duration,date:date});
+  res.json({"_id":id,"username":user.username,"date":date,"duration":duration,"description":description});
 });
 
-// Get Exercise Log
+// Get Exercise log
 app.get("/api/users/:_id/logs", function(req, res) {
   let id = req.params._id;
   let user = users[id];
-  res.json({"_id":id, "username":user.Username, "count":user.Log.length, "log":user.Log});
+  res.json({"_id":id, "username":user.username, "count":user.log.length, "log":user.log});
 });
 
+// Get All Users
+app.get("/api/users", function(req, res) {
+  let response = [];
+  for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    response.push({"_id":user._id,"username":user.username/*,"__v":0*/});
+  }
+  res.send(response);
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
